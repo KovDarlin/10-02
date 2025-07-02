508671f1b82e8a9f065628fce7ff2607d3cb113f
KovDarlin <ki27.kovalenko.daryna@vtc.vn.ua>
2025-07-02 22:25:28 +0300

    done

--- Program.cs
+++ Program.cs
@@ -1,1 +1,14 @@
-﻿
+﻿using System;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        int[] list = { -34, 89, 21, 70, 7, 14, 12 };
+        Func<int, int, int> result = (min,max) => list.Count(x => x >= min && x <= max);
+        Console.WriteLine("(-30, 90) :" + result(-30,90));
+        Console.WriteLine("(5, 30) :" + result(5, 30));
+
+    }
+}

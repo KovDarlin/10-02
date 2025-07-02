06104687ce74e8a95f99b7b4b4fee7a5038f97f8
KovDarlin <ki27.kovalenko.daryna@vtc.vn.ua>
2025-07-02 22:19:59 +0300

    done

--- Program.cs
+++ Program.cs
@@ -1,1 +1,14 @@
-﻿
+﻿using System;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        int[] list = { 34, 89, 21, 70, 7, 14, 12 };
+        Func<int, int> result = res => list.Count(x => x % res == 0);
+        Console.WriteLine("7 :" + result(7));
+        Console.WriteLine("3 :" + result(3));
+        Console.WriteLine("2 :" + result(2));
+    }
+}

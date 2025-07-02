549c09d34c4d169df50d9d56ce54792d673c578a
KovDarlin <ki27.kovalenko.daryna@vtc.vn.ua>
2025-07-02 22:56:02 +0300

    done

--- Program.cs
+++ Program.cs
@@ -1,1 +1,21 @@
-﻿
+﻿using System;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        int[] list = { -34, -89, -21, -70, 7, 14, -12, -70, 89, 7, 7, -70 };
+
+        var negative = list.Where(n => n < 0).Distinct().OrderBy(n => n).ToArray();
+
+        Action Print = () =>
+        {
+            Console.WriteLine("Unique negative numbers:");
+            foreach (var n in negative)
+                Console.WriteLine(n);
+        };
+
+        Print();
+    }
+}
